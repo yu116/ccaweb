af34cc0367a5284c18b4fcb40506c40c3aa99d4d
Glenda Tan <glenda.tan04@rafflesgirlssch.edu.sg>
2021-07-14 10:29:37 +0800

    add

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Computer Science Society'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Raffles CSS'
 
 export default function Layout({ children, home }) {
   return (
