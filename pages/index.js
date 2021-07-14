292e17af224abfd3f1bd2c2353a26e3181fea554
xyntechx <xyntechx@gmail.com>
2021-07-14 10:36:11 +0800

    Home Screen Basic Prototype

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-// import Link from 'next/link'
-// import Date from '../components/date'
 
 export default function Home() {
   return (
@@ -11,6 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd, utilStyles.screen}>
+        <h1>Computer Science Society</h1>
         <p>description</p>
         <p>
           (test{' '}
@@ -20,12 +19,3 @@ export default function Home() {
     </div>
   )
 }
-
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData()
-//   return {
-//     props: {
-//       allPostsData
-//     }
-//   }
-// }
