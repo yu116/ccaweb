71c58a1955b852eeeed06fb4894b6230ccbf9658
xyntechx <xyntechx@gmail.com>
2021-07-21 16:48:01 +0800

    Update Activities/Courses page

--- pages/activities-courses.js
+++ pages/activities-courses.js
@@ -1,14 +1,43 @@
 import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
 
 export default function ActivitiesAndCourses() {
     return (
-        <>
-        <h1>Activities/Courses</h1>
-        <h2>
-            <Link href="/">
-            <a>Back to home</a>
-            </Link>
-        </h2>
-        </>
+
+        <div id="activities-courses" className={utilStyles.screen}>
+            <section className={utilStyles.headingMd}>
+            <h1 className={utilStyles.title}>2021-2022 Activities/Courses</h1>
+                <div className={utilStyles.container}>
+                    <p>
+                    📦 Backend Development
+                    </p>
+                    <p>
+                    🔥 Competitive Programming
+                    </p>
+                    <p>
+                    🤖 Data Science and Machine Learning
+                    </p>
+                    <p>
+                    💭 Incubator Programmes
+                    </p>
+                    <p>
+                    🎨 UI/UX Design
+                    </p>
+                    <p>
+                    📱 Web and Mobile App Development
+                    </p>
+                </div>
+
+                <br></br>
+
+                <div className={utilStyles.subscreen}>
+                <Link href="/">
+                <a className={utilStyles.btn}>
+                    Back to Home
+                </a>
+                </Link>
+                </div>
+            </section>
+        </div>
     )
 }
