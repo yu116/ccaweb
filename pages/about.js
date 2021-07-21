f4ff5e2ee5805445307362cda7390f1a848589a3
xyntechx <xyntechx@gmail.com>
2021-07-21 10:37:33 +0800

    Move About page to its own path

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,38 @@
 import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
 
 export default function About() {
     return (
-        <>
-        <h1>About</h1>
-        <h2>
-            <Link href="/">
-            <a>Back to home</a>
-            </Link>
-        </h2>
-        </>
+        <div className={utilStyles.screen, utilStyles.screenalt}>
+            <section className={utilStyles.headingMd}>
+                <h1 className={utilStyles.title, utilStyles.titlealt}>About Us</h1>
+                <div className={utilStyles.container}>
+                    <p>
+                    💭 "The ultimate promise of technology is to make us masters of a world that we command by the push of a button"
+                    </p>
+                    <p>
+                    👤 a CCA filled with passionate individuals who believe in the power of technology in transforming our way of life for the better
+                    </p>
+                    <p>
+                    🗓 Sessions held every Wednesday from 9 to 11am
+                    </p>
+                    <p>
+                    🎓 Offers courses from Data Science to Web Development
+                    </p>
+                    <p>
+                    🏆 Organises and participates in competitions and hackathons
+                    </p>
+                </div>
+
+                <br></br>
+
+                <Link href="/">
+                <a className={utilStyles.btn}>
+                    Back to Main Screen
+                </a>
+                </Link>
+
+            </section>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
@@ -48,42 +48,8 @@ export default function Home() {
                 Contact Us
               </a>
             </Link>
+            
           </div>
-
-
-
-
-
-        </section>
-      </div>
-
-      <div id="about-us" className={utilStyles.screen, utilStyles.screenalt}>
-        <section className={utilStyles.headingMd}>
-          <h1 className={utilStyles.title, utilStyles.titlealt}>About Us</h1>
-          <div className={utilStyles.container}>
-            <p>
-              💭 "The ultimate promise of technology is to make us masters of a world that we command by the push of a button"
-            </p>
-            <p>
-              👤 a CCA filled with passionate individuals who believe in the power of technology in transforming our way of life for the better
-            </p>
-            <p>
-              🗓 Sessions held every Wednesday from 9 to 11am
-            </p>
-            <p>
-              🎓 Offers courses from Data Science to Web Development
-            </p>
-            <p>
-              🏆 Organises and participates in competitions and hackathons
-            </p>
-          </div>
-
-          <br></br>
-
-          <a className={utilStyles.btn} href="#main">
-              Back to Main Screen
-          </a>
-
         </section>
       </div>
     </div>
