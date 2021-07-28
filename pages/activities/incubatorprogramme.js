30f9a9ceae735a6247c70c53a0116e13d1328b9f
Kannan Janani <janani.kannan@frankmax.digital>
2021-07-28 09:06:47 +0800

    test

---
+++ pages/activities/incubatorprogramme.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function incubator() {
+    return (
+        <>
+        <h1>Incubator</h1>
+        <h2>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </h2>
+        </>
+    )
+}
---
+++ pages/activities/ui-uxdesign.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+function ui() {
+    return (
+        <>
+        <h1>UI/UX Design</h1>
+        <h2>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </h2>
+        </>
+    )
+}
+
+export default ui;
