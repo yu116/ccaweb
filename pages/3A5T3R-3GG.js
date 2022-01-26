69dcda7ae94a8a157faf5ade97ef2db11d001405
DAVID CHAN <22YDAVI339G@student.ri.edu.sg>
2022-01-26 10:46:13 +0800

    secret

---
+++ pages/3A5T3R-3GG.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import Image from 'next/image'
+
+function Easteregg() {
+    return (
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/x31tDT-4fQw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    )
+}
+
+export default Easteregg;
