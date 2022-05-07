e6d7e27b5b1103dba9eb2c0370558119845a8daf
Luckder <davidchan3g@gmail.com>
2022-05-07 20:35:32 +0800

    Added footer to testimonial

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -42,6 +42,42 @@ function Testimonials() {
                     </a>
                     </Link>
                 </div>
+
+                <section className={utilStyles.footer}>
+                <div className={utilStyles.footercontent}>
+                    <div className={utilStyles.li}>
+                        <span className="material-icons">place</span>
+                        <Link href="http://www.ri.edu.sg/visit-us">
+                        <a target="_blank" className={utilStyles.link}>Our Location</a>
+                        </Link>
+                    </div>
+
+                    <div className={utilStyles.li}>
+                        <span className="material-icons">mail</span>
+                        <Link href="mailto:cssrjc@gmail.com">
+                        <a target="_blank" className={utilStyles.link}>cssrjc@gmail.com</a>
+                        </Link>
+                    </div>
+
+                    <div className={utilStyles.li}>
+                        <span className="material-icons">alternate_email</span>
+                        <Link href="https://www.instagram.com/rafflescss/">
+                        <a target="_blank" className={utilStyles.link}>rafflescss</a>
+                        </Link>
+                    </div>
+
+                    <div className={utilStyles.li}>
+                        <span className="material-icons">photo_camera</span>
+                        <Link href="https://www.instagram.com/rafflescss/">
+                        <a className={utilStyles.link}>Photos</a>
+                        </Link>
+                    </div>
+                </div>
+
+                <hr></hr>
+
+                <p>&#169; 2021 Raffles Institution Computer Science Society</p>
+            </section>
         </div>
     )
 }
